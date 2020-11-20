b8799e51c0da7ed1e5d82d954d49fc474238cdbb
DillPickle80 <67298238+DillPickle80@users.noreply.github.com>
2020-11-20 10:35:05 -0500

    Working

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-const express = require('express');
-const app = express();
+var eventServices = require("./eventServices");
 
-app.get('/', (req, res) => {
-  res.send('Hello from Dylan Lepage and App Engine!');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World From Dylan! Go to /events to get the events!");
+});
+
+app.get("/events", (req, res) => {
+  res.send(eventServices.events);
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
